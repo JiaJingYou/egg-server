7642ee8ea29350fb04fbd720e677edb1ec637e02
youjiajing <237390045@qq.com>
2020-06-02 22:34:32 +0800

    UserController和swagger

---
+++ egg-server/config/config.default.js
@@ -0,0 +1,45 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1591105054806_8169';
+
+  // add your middleware config here
+  config.middleware = [];
+  config.swaggerdoc = {
+    dirScanner: './app/controller',
+    apiInfo: {
+      title: 'egg接口',
+      description: 'egg接口 swagger-ui for egg',
+      version: '1.0.0',
+    },
+    schemes: ['http', 'https'],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    enableSecurity: false,
+    // enableValidate: true, // 自动接口校验，
+    routerMap: true, // 自动注册路由
+    enable: true,
+  }
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
